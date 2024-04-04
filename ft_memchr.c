3bde59c4cadfc5f40a0cd260716984fc513832e2
nero167 <nerojan7@hotmail.com>
2024-04-04 15:20:25 +0200

    added

--- ft_memchr.c
+++ ft_memchr.c
@@ -1,22 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: namirtha <namirtha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/11 10:31:37 by namirtha          #+#    #+#             */
-/*   Updated: 2024/04/02 16:56:41 by namirtha         ###   ########.fr       */
+/*   Created: 2024/04/02 16:22:51 by namirtha          #+#    #+#             */
+/*   Updated: 2024/04/02 17:02:18 by namirtha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strchr(const char *s, int c)
+void	*ft_memchr(const void *s, int c, size_t n)
 {
-	while (*s && *s != (char)c)
-		s++;
-	if (*s == (char)c || !c)
-		return ((char *)s);
+	const unsigned char	*ptr;
+
+	ptr = (unsigned char *)s;
+	while (n--)
+	{
+		if (*ptr == (unsigned char)c)
+			return ((void *)ptr);
+		ptr++;
+	}
 	return (NULL);
 }
--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,34 +6,34 @@
 /*   By: namirtha <namirtha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/14 19:12:59 by namirtha          #+#    #+#             */
-/*   Updated: 2024/03/18 19:49:34 by namirtha         ###   ########.fr       */
+/*   Updated: 2024/04/02 16:58:34 by namirtha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*strnstr(const char *haystack, const char *needle, size_t len)
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-	size_t		i;
-	int			j;
+	size_t	needle_len;
+	size_t	haystack_len;
 
-	i = 0;
-	if (needle)
+	if (!*needle)
 		return ((char *)haystack);
-	while (i < len && haystack[i] != '\0')
+	if (len == 0)
+		return (NULL);
+	needle_len = ft_strlen(needle);
+	haystack_len = ft_strlen(haystack);
+	if (len > haystack_len)
+		len = haystack_len;
+	while (len >= needle_len)
 	{
-		while (haystack[i + j] == needle[j]
-			&& needle[j] != '\0' && (i + j) < len)
-		{
-			i++;
-		}
-		if (needle[j] == '\0')
-			return ((char *)&haystack[i]);
-		i++;
-		j = 0;
+		if (ft_strncmp(haystack, needle, needle_len) == 0)
+			return ((char *)haystack);
+		haystack++;
+		len--;
 	}
 	return (NULL);
 }
 
-//char *haystack = "Hello_1";
-//char *ned = "_1";
+// char *haystack = "Hello_1";
+// char *ned = "_1";
